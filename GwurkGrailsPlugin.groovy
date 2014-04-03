e7242e7ce73d0bcd6eb8a59337823d2db9d9c5fa
jintrone <jintrone@msu.edu>
2014-04-03 11:46:26 -0400

    update version stamp for new release

--- GwurkGrailsPlugin.groovy
+++ GwurkGrailsPlugin.groovy
@@ -4,7 +4,7 @@ import org.apache.shiro.crypto.hash.Sha256Hash
 
 class GwurkGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "0.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3 > *"
     // resources that are excluded from plugin packaging
